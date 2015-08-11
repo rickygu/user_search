f464cd53f85778d256f4aa8d1270f842d7cb8306
rickyzgu <rickyzgu@gmail.com>
2015-08-10 22:57:30 -0700

    case insensitive like for pg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
       @users = User.paginate(:page => params[:page], :per_page => 12)
     else
 
-      @users = User.where("name LIKE ? or line1 LIKE ? or line2 LIKE ? or city LIKE ? or state LIKE ? or zip LIKE ? or phone LIKE ?", "%#{@q}%","%#{@q}%","%#{@q}%","%#{@q}%","%#{@q}%","%#{@q}%","%#{@q}%").paginate(:page => params[:page], :per_page => 12)
+      @users = User.where("name ILIKE ? or line1 ILIKE ? or line2 ILIKE ? or city ILIKE ? or state ILIKE ? or zip ILIKE ? or phone ILIKE ?", "%#{@q}%","%#{@q}%","%#{@q}%","%#{@q}%","%#{@q}%","%#{@q}%","%#{@q}%").paginate(:page => params[:page], :per_page => 12)
     end
   end
 
